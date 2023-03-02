fb679cdfa710add6540232ae5cba50b8de1a5274
Vanderlei <111476839+AlostXD@users.noreply.github.com>
2023-03-02 16:46:11 -0300

    Feito o Dropdown Menu
    Foi "Finalizado" o Dropdown Menu faltando apenas colocar um estilo melhor.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-let dropdownMenu = document.getElementById("dropdown");
+let dropdownMenu = document.getElementById("dropdown-categoria");
 let dropitem = document.getElementById("dropdown-item");
+let dropdownArrow = document.getElementById("seta-dropdown")
+let dropstatus = false;
 
 dropdownMenu.addEventListener('click', () => {
-    dropitem.style.display = "block";
-    dropitem.style.color = "#000"
+    console.log(dropdownMenu)
+    if (dropstatus == false) {
+        console.log("mostrando dropdown")
+        dropitem.style.display = "flex";
+        dropitem.style.flexDirection = "Column";
+        dropitem.style.color = "#000";
+        dropstatus = true;
+        dropdownArrow.style.rotate = "90deg";
+    }
+    else if (dropstatus == true) {
+        console.log("escondendo o texto")
+        dropitem.style.display = "none";
+        dropdownArrow.style.rotate = "-0deg";
+        dropstatus = false;
+    }
 }) 
