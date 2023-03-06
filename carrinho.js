0ce7943d4d27432400e84828e6b939688fc8f9e8
Vanderlei <111476839+AlostXD@users.noreply.github.com>
2023-03-06 15:00:18 -0300

    Carrinho
    Feito o carrinho faltando apenas o botão de remover item

--- carrinho.js
+++ carrinho.js
@@ -3,16 +3,32 @@ const carrinho = [
     {
         nome: "Máscara com elástico ",
         preco: 29.90,
-        precoPix: 27,
         imagem: "img/mascaracomelastico.png",
         id: 1
     },
     {
         nome: "Máscara com elástico ",
         preco: 29.90,
-        precoPix: 27,
         imagem: "img/mascaracomelastico.png",
         id: 2
+    },
+    {
+        nome: "Máscara com elástico ",
+        preco: 29.90,
+        imagem: "img/mascaracomelastico.png",
+        id: 3
+    },
+    {
+        nome: "Máscara com elástico ",
+        preco: 29.90,
+        imagem: "img/mascaracomelastico.png",
+        id: 4
+    },
+    {
+        nome: "Máscara com elástico ",
+        preco: 29.90,
+        imagem: "img/mascaracomelastico.png",
+        id: 5
     }
 ]
 var mensagem = `Olá, tenho interesse nos seguintes itens: `;
@@ -26,33 +42,39 @@ btnFazerPedido.addEventListener("click", () => {
 
 
 
-const ulCarrinho = document.getElementById("carrinho-lista");
-carrinho.forEach(produto => {
-    const liProduto = document.createElement("li");
-    liProduto.className = "carrinho-item";
-    liProduto.innerHTML = `
-    <img src=${produto.imagem} alt=${produto.nome}>
-    <div class="carrinho-item-text">
-        <h3>
-            ${produto.nome}
-        </h3>
-        <span>R$ ${produto.preco}</span>
-        <div>
-            <a href="#" id="carrinho-item-menos">-</a>
-            <input type="text" value=1>
-            <a href="#" id="carrinho-item-mais">+</a>
+
+
+function montaCarrinho(){
+    let elementoValorCarrinho = document.getElementById("valor-total")
+    let valorCarrinho = 0;
+    const ulCarrinho = document.getElementById("carrinho-lista");
+    carrinho.forEach(produto => {
+        valorCarrinho = valorCarrinho + produto.preco;
+        const liProduto = document.createElement("li");
+        liProduto.className = "carrinho-item";
+        liProduto.innerHTML = `
+        <img src=${produto.imagem} alt=${produto.nome}>
+        <div class="carrinho-item-text">
+            <h3>
+                ${produto.nome}
+            </h3>
+            <span id="preco-produto">R$ ${produto.preco}</span>
+            <div>
+                <a href="#" id="carrinho-item-menos">-</a>
+                <input type="text" value=1>
+                <a href="#" id="carrinho-item-mais">+</a>
+            </div>
         </div>
-    </div>
-`
-const barra = document.createElement("hr");
-    ulCarrinho.appendChild(liProduto);
-    ulCarrinho.appendChild(barra)
+    `
+        ulCarrinho.appendChild(liProduto);
+        elementoValorCarrinho.innerText = "R$ " + valorCarrinho.toFixed(2)
+    })
 
-})
+}
 
-
+montaCarrinho();
 
 /* ADICIONA AO CARRINHO */
-const qtdCarrinho = document.getElementById("qtd-carrinho");
-window.localStorage.setItem("qtdCarrinho", carrinho.length);
+const qtdCarrinho = document.getElementById("numero-carrinho");
+//window.localStorage.setItem("qtdCarrinho", carrinho.length);
 qtdCarrinho.innerText = carrinho.length;
