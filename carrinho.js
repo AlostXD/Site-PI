39384a7d7685c14f930e13bfca6a41bfe22356bb
Vanderlei <111476839+AlostXD@users.noreply.github.com>
2023-03-08 15:54:08 -0300

    Arrumado o Dropdown no Carrinho
    .

--- carrinho.js
+++ carrinho.js
@@ -74,6 +74,29 @@ function montaCarrinho(){
 
 montaCarrinho();
 
+let dropdownMenu = document.getElementById("dropdown-categoria");
+let dropitem = document.getElementById("dropdown-item");
+let dropdownArrow = document.getElementById("seta-dropdown")
+let dropstatus = false;
+
+dropdownMenu.addEventListener('click', () => {
+    console.log(dropdownMenu)
+    if (dropstatus == false) {
+        console.log("mostrando dropdown")
+        dropitem.style.display = "flex";
+        dropitem.style.flexDirection = "Column";
+        dropitem.style.color = "#000";
+        dropstatus = true;
+        dropdownArrow.style.rotate = "90deg";
+    }
+    else if (dropstatus == true) {
+        console.log("escondendo o texto")
+        dropitem.style.display = "none";
+        dropdownArrow.style.rotate = "-0deg";
+        dropstatus = false;
+    }
+})
+
 /* ADICIONA AO CARRINHO */
 const qtdCarrinho = document.getElementById("numero-carrinho");
 window.localStorage.setItem("qtdCarrinho", carrinho.length);
